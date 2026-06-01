e6502fa418b5371a386088fc48b737b4042fe41d
Shivansh Mishra <shivnsh01@gmail.com>
2026-06-02 00:18:47 +0530

    Added a simple program to print name twice

---
+++ code/src/nameTwice.java
@@ -0,0 +1,13 @@
+// Java 25
+// void main(){
+//     System.out.println("Shivansh");
+//     System.out.println("Shivansh");
+// }
+
+//Java 17
+public class nameTwice {
+    public static void main(String [] args){
+        System.out.println("Shivansh");
+        System.out.println("Shivansh");
+    }
+}
