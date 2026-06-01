1e2689bdf73c9a2c00a34683a815a62968c1b869
Shivansh Mishra <shivnsh01@gmail.com>
2026-06-02 00:43:17 +0530

    Implemented comments, and updated notes

---
+++ code/src/comments.java
@@ -0,0 +1,23 @@
+// 4.4 Challenge 2
+
+void main() {
+    IO.println("A");
+    //IO.println("B");
+    IO.println("C");//
+    IO.println("D");
+    /*
+    IO.println("E");
+    IO.println("F");*/
+    IO.println("G");
+}
+
+// OUTPUT must be:
+// A 
+// C 
+// D 
+// G
+
+//New learning:
+// In modern Java, we need to use " java -cp location filename"
+// ex: java -cp code\src comments
+// to be able to run those class files.
--- learnings/notes.md
+++ learnings/notes.md
@@ -10,3 +10,6 @@ void main(){
 
 java Main.java or java src/Main.java (if not in current working directory)
 
+3. In modern Java, we need to use " java -cp location filename"
+ ex: java -cp code\src comments
+to be able to run those class files. Here -cp means "Look for the class files"
